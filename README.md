ebd58fae86461126f360249c09d4799b047b1f20
thank243 <thank243@gmail.com>
2021-12-21 06:00:07 +0800

    After receive /url will delete message for privacy.

--- README.md
+++ README.md
@@ -1,18 +1,13 @@
-### 配置说明
+### Demo： [@stairunlock_test_bot](https://t.me/stairunlock_test_bot)
 
+### 配置说明
 ````yaml
-// subconvert API 地址
-converterAPI: https://api.dler.io
-// 节点检测同时最大连接数
-maxConn: 256
-// 最大同时在线用户
-maxOnline: 10                      
-// 日志等级: debug/info/warning/error/silent
-log_level: info
-// 检测间隔时间(单位: 分钟)
-internal: 5
-// 机器人token
-telegramToken:                    
+converterAPI: https://api.dler.io   # subconverter API 地址
+maxConn: 256                        # 节点检测同时最大连接数  
+maxOnline: 10                       # 最大同时在线用户 
+log_level: info                     # 日志等级: debug/info/warning/error/silent
+internal: 5                         # 检测间隔时间(单位: 分钟)
+telegramToken:                      # 机器人token 
 ````
 
 ### 命令参数
@@ -23,5 +18,3 @@ Usage of StairUnlocker-Bot:
   -h	this help
   -v	show current version of StairUnlock
 ````
-
-### 有个测试bot： [@stairunlock_test_bot](https://t.me/stairunlock_test_bot)  介意的勿用。
--- user/common.go
+++ user/common.go
@@ -6,11 +6,12 @@ import (
 	"time"
 )
 
-func (u *User) Send(ctx string) (err error) {
-	_, err = u.Bot.Send(tg.NewMessage(u.ID, ctx))
+func (u *User) Send(ctx string) (resp tg.Message, err error) {
+	resp, err = u.Bot.Send(tg.NewMessage(u.ID, ctx))
 	if err != nil {
 		return
 	}
+	u.MessageID = resp.MessageID
 	return
 }
 
@@ -18,9 +19,25 @@ func (u *User) UserOutInternal(n int) bool {
 	internal := time.Duration(n)
 	if time.Since(time.Unix(u.Data.LastCheck, 0)) < internal*time.Minute {
 		remainTime := internal*time.Minute - time.Since(time.Unix(u.Data.LastCheck, 0))
-		_ = u.Send(fmt.Sprintf("Please try again after %s.", remainTime.Round(time.Second)))
+		_, _ = u.Send(fmt.Sprintf("Please try again after %s.", remainTime.Round(time.Second)))
 		return false
 	} else {
 		return true
 	}
+}
+
+func (u *User) DeleteMessage(msgID int) error {
+	_, err := u.Bot.Send(tg.NewDeleteMessage(u.ID, msgID))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (u *User) EditMessage(msgID int, text string) error {
+	_, err := u.Bot.Send(tg.NewEditMessageText(u.ID, msgID, text))
+	if err != nil {
+		return err
+	}
+	return nil
 }
