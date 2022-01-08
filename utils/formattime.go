04a83b1ba9e37a964d5e683ea8df241d071c0d5b
thank243 <thank243@gmail.com>
2022-01-08 22:50:28 +0800

    add /version command, show version, online user and uptime information.

---
+++ utils/formattime.go
@@ -0,0 +1,17 @@
+package utils
+
+import (
+	"fmt"
+	"math"
+	"time"
+)
+
+func FormatTime(t time.Duration) string {
+	timeStr := t.Round(time.Second).String()
+	if t-24*time.Hour > 0 {
+		day := int(math.Floor(float64(t / (24 * time.Hour))))
+		timeStr = (t - time.Duration(day*24)*time.Hour).String()
+		timeStr = fmt.Sprintf("%dd%s", day, timeStr)
+	}
+	return timeStr
+}
